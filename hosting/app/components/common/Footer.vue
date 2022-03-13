9d8e628cbd9dc7dc7e309377a0682b1b18783055
森元心 <shinshinshin0914@gmail.com>
2022-03-13 20:57:18 +0900

    ヘッダー、フッター作成

---
+++ hosting/app/components/common/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-footer :absolute="!fixed" app>
+    <span>&copy; {{ currentYear }}</span>
+  </v-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const currentYear = computed(() => new Date().getFullYear())
+    const fixed = false as const
+    return {
+      currentYear,
+      fixed,
+    }
+  },
+})
+</script>
+x
