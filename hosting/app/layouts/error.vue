9a411b6999ea4e0f195d4161541b4eda86c71777
森元心 <shinshinshin0914@gmail.com>
2022-03-13 20:57:28 +0900

    エラーページ作成

--- hosting/app/layouts/error.vue
+++ hosting/app/layouts/error.vue
@@ -1,45 +1,54 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <v-container class="container">
+      <v-row>
+        <v-col cols="12" align="center">
+          <h1 v-if="error.statusCode === 404">404 エラー</h1>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" align="center">
+          <v-card class="error_card pa-4">
+            <v-card-title>{{ title }}</v-card-title>
+            <v-btn outline dark color="blue" to="/"> ホームページへ</v-btn>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
-<script>
-export default {
-  name: 'EmptyLayout',
-  layout: 'empty',
+<script lang="ts">
+import { defineComponent, useMeta, computed } from '@nuxtjs/composition-api'
+
+export default defineComponent({
   props: {
     error: {
       type: Object,
-      default: null
+      default: null,
+    },
+  },
+  setup(props) {
+    useMeta(() => ({ title: '取引先一覧' }))
+    const title = computed(() =>
+      props.error.statusCode === 404
+        ? 'お探しのページは見つかりませんでした。'
+        : '原因不明のエラーが発生しました。'
+    )
+    return {
+      title,
     }
   },
-  data () {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
-  },
-  head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
-    }
-  }
-}
+  head: {},
+})
 </script>
 
 <style scoped>
 h1 {
   font-size: 20px;
+}
+.error_card {
+  min-width: 300px;
+  width: 50%;
 }
 </style>
