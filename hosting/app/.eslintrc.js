37cae8cbe0d5a8ff48c2b047df1e7389e6dc596b
森元心 <shinshinshin0914@gmail.com>
2022-03-21 22:13:56 +0900

    firebase繋ぎこみ

--- hosting/app/.eslintrc.js
+++ hosting/app/.eslintrc.js
@@ -2,18 +2,18 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
-    'prettier'
+    'prettier',
   ],
-  plugins: [
-  ],
+  plugins: [],
   // add your custom rules here
   rules: {
     'vue/multi-word-component-names': 0,
+    'no-console': 'off', // TODO 一旦無効にするけど、ロガー入れたら有効にするぞ
+    'import/no-duplicates': 'off'
   },
-
 }
